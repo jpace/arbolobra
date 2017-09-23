cfa5f2792cdfd2eec26779d2c9751b14cfb29c2a
Jeff Pace <jeugenepace@gmail.com>
2017-09-23 12:48:18 -0400

    Create a tree from a set of lines and separators.

---
+++ lib/treeify/tree.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/ruby -w
+# -*- ruby -*-
+
+require 'logue/loggable'
+
+module Treeify
+end
+
+class Treeify::Node
+  include Comparable
+  
+  attr_reader :value
+  attr_reader :children
+  
+  def initialize value, children = Array.new
+    @value = value
+    @children = children
+  end
+
+  def write indent = 0, chars = nil
+    printf "%*s%s\n", indent * 2, "", @value
+    @children.each do |child|
+      child.write indent + 1
+    end
+  end
+
+  def to_s
+    @value
+  end
+
+  def <=> other
+    value <=> other.value && children <=> other.children
+  end
+end
+
+class Treeify::Tree
+  include Logue::Loggable
+
+  attr_reader :root
+  attr_reader :nodes
+  
+  def initialize lines: nil, separator: nil
+    if lines
+      raise "lines argument requires a separator" unless separator
+      @root = Treeify::Node.new nil
+      
+      lines.each do |line|
+        elements = line.split separator
+        create_nodes elements
+      end
+    end
+  end
+
+  def create_nodes elements
+    node = @root
+    elements.each do |elmt|
+      subnode = node.children[-1]
+      if subnode && subnode.value == elmt
+        node = subnode
+      else
+        subnode = Treeify::Node.new elmt
+        node.children << subnode
+        node = subnode
+      end
+    end
+  end
+end
---
+++ test/treeify/tree_test.rb
@@ -0,0 +1,76 @@
+#!/usr/bin/ruby -w
+# -*- ruby -*-
+
+require 'test/unit'
+require 'treeify/tree'
+require 'pp'
+require 'paramesan'
+
+Logue::Log.level = Logue::Log::DEBUG
+
+class TreeTest < Test::Unit::TestCase
+  include Logue::Loggable
+  include Paramesan
+
+  def self.newnode value, *children
+    Treeify::Node.new value, children
+  end
+
+  def self.build_params
+    Array.new.tap do |params|
+      lines = Array.new
+      lines << "abc"
+      root = newnode nil, [ newnode("abc") ]
+      
+      params << [ newnode(nil,
+                          newnode("abc")),
+                  lines ]
+
+      lines = Array.new
+      lines << "abc"
+      lines << "def"
+
+      params << [ newnode(nil,
+                          newnode("abc"),
+                          newnode("def")),
+                  lines ]
+
+      lines = Array.new
+      lines << "abc/def/ghi"
+      lines << "abc/def/jkl"
+      lines << "abc/ghi"
+      lines << "def/ghi"
+      lines << "def/ghi/2"
+      lines << "def/ghi/3"
+      lines << "abc/mno"
+
+      root = newnode(nil,
+                     newnode("abc",
+                             newnode("def",
+                                     newnode("ghi"), newnode("jkl")),
+                             newnode("ghi")),
+                     
+                     newnode("def",
+                             newnode("ghi",
+                                     newnode("2"),
+                                     newnode("3"))),
+
+                     newnode("abc",
+                             newnode("mno")))
+
+      params << [ root, lines ]
+      
+    end
+  end
+
+  param_test build_params do |exp, lines|
+    # exp.write
+    
+    tree = Treeify::Tree.new lines: lines, separator: "/"
+    root = tree.root
+
+    # root.write
+
+    assert_equal exp, root, "lines: #{lines}"
+  end
+end
